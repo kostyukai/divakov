c1538195cfd922a379ac0b228c33c523ac179bd9
divserge <div.serge@gmail.com>
2014-03-29 09:01:04 +0400

    added task 20

---
+++ hw-1/task_20.s
@@ -0,0 +1,66 @@
+.data
+
+.comm array, 80, 32
+
+int1:
+	.space 4
+str_fmt:
+	.string "%d"
+
+str_out:
+	.string "%d\n"
+
+.text
+.globl main
+
+main:
+	pushl %ebp
+	movl %esp, %ebp
+//prologue
+	pushl $int1
+	pushl $str_fmt
+	call scanf
+	add $8, %esp
+// read the number of elements
+	movl int1, %ecx
+//read the elements
+readarr:
+	pushl %ecx
+	movl $array, %ebx
+	movl %ecx, %edx
+	imull $4, %edx
+	addl %edx, %ebx
+	pushl %ebx
+	pushl $str_fmt
+	call scanf
+	addl $8, %esp
+	popl %ecx
+	loop readarr
+
+//reset the loop
+	movl $array+4, %ebx
+	movl $array, %eax
+	movl int1, %ecx 	
+find:
+	addl $4, %eax
+	cmpl %ebx, (%eax)
+	jl new
+cnt:
+	loop find
+	jmp fin
+new:
+	movl (%eax), %ebx
+	jmp cnt
+fin:	
+	pushl %ebx
+	pushl $str_fmt
+	call printf
+	addl $8, %esp
+
+//epilogue
+	movl %ebp, %esp
+	popl %ebp
+	ret
+	
+	
+ 
