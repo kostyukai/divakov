f7145cf996e0c49987baaef0e97bab599647f7a4
divserge <div.serge@gmail.com>
2014-03-29 22:18:10 +0400

    added task 10

---
+++ hw-1/task_10.s
@@ -0,0 +1,61 @@
+.data
+
+int_t:
+	.space 4
+int_1:
+	.space 4
+char_1:
+	.space 1
+float_1:
+	.space 8
+
+str_fmtr:
+	.string "%d %c %lf"
+str_fmti:
+	.string "%d"
+str_fmtc:	
+	.string "%c"
+str_fmtf:
+	.string "%lf"
+
+
+.text
+.globl main
+main:
+
+//prologue
+	pushl %ebp
+	movl %esp, %ebp
+
+	pushl $float_1
+	pushl $char_1
+	pushl $int_1
+	pushl $str_fmtr
+	call scanf
+	addl $8, %esp
+
+	movl int_1, %eax
+	pushl %eax
+	pushl $str_fmti
+	call printf
+	addl $8, %esp
+
+	movb char_1, %al
+	pushl %eax
+	pushl $str_fmtc
+	call printf
+	addl $8, %esp
+
+	fldl float_1
+	movl $str_fmtf, %eax
+	fstpl 4(%esp)
+	movl %eax, (%esp)
+	call printf
+	addl $12, %esp
+	
+	
+	movl $0, %eax
+	movl %ebp, %esp
+	popl %ebp
+	ret
+	
